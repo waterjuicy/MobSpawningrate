6b35eb74274deff7ad80b99240795c318716a30e
waterjuicy <waterjucey@gmail.com>
2026-04-23 19:40:45 +0900

    Create MobSpawningListener event listener class

---
+++ src/main/java/com/waterjuicy/mobspawningrate/MobSpawningListener.java
@@ -0,0 +1,13 @@
+package com.waterjuicy.mobspawningrate;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.CreatureSpawnEvent;
+
+public class MobSpawningListener implements Listener {
+
+    @EventHandler
+    public void onMobSpawn(CreatureSpawnEvent event) {
+        // Add your logic here to handle mob spawning
+    }
+}

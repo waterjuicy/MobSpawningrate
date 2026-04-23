599a3a44630b38f34235c2cb97c6eb8585f08a93
waterjuicy <waterjucey@gmail.com>
2026-04-23 19:41:02 +0900

    Create ScalingManager class

---
+++ src/main/java/com/waterjuicy/mobspawningrate/ScalingManager.java
@@ -0,0 +1,9 @@
+package com.waterjuicy.mobspawningrate;
+
+public class ScalingManager {
+    // Scaling logic here
+
+    public void scaleMobSpawnRate(int baseRate, int scalingFactor) {
+        // Implementation of the mob spawning rate scaling logic
+    }
+}

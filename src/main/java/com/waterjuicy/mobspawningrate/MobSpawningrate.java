b589b3a5ed4f8f53146e53d5a8fdcf91e27a63e5
waterjuicy <waterjucey@gmail.com>
2026-04-23 19:40:56 +0900

    Create main plugin class for MobSpawningrate

---
+++ src/main/java/com/waterjuicy/mobspawningrate/MobSpawningrate.java
@@ -0,0 +1,15 @@
+package com.waterjuicy.mobspawningrate;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MobSpawningrate extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
